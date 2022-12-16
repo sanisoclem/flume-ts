0f2968391877c77b3eaf552f6b0beaf17fa45df6
Jerahmeel Cosinas <jcosinas@creditclear.com.au>
2022-12-17 01:58:40 +1100

    take 2 using rollup

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import packageJson from './package.json' assert { type: "json" };
+import { fileURLToPath } from 'url';
+//https://github.com/rollup/plugins/issues/1366
+const __filename = fileURLToPath(import.meta.url);
+global['__filename'] = __filename;
+import terser from "@rollup/plugin-terser";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+    ],
+    external: ["react", "react-dom", "styled-components"],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
